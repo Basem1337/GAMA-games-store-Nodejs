84484e5001fe09169c2a2e0bb7c1c5d2474d9d4f
Basem Mohamed <basem.m1337@gmail.com>
2025-04-11 01:03:42 +0200

    fix: token issue

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,7 +1,7 @@
 const JWT = require("jsonwebtoken")
 
 async function isAuthenticated(req,res,next){
-    const token = req.headers.authorization?.split(" ")[2]
+    const token = req.headers.authorization?.split(" ")[1]
 
     if(!token){
         return res.status(401).json("Unauthorized")
